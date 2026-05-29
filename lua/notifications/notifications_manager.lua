d1ca0cf323f028914307e8a55c20d9d5ffa18cdd
Nick Kadutskyi <nick@kadutskyi.com>
2026-05-28 17:52:40 -0700

    wip

--- lua/notifications/notifications_manager.lua
+++ lua/notifications/notifications_manager.lua
@@ -1,5 +1,6 @@
 local utils = require("notifications.utils")
 local conf = require("notifications.config")
+local NotificationDisplayType = require("notifications.notification_display_type")
 
 local NotificationManagerClass = {}
 local NotificationManager = {}
@@ -28,11 +29,26 @@ end
 
 ---@private
 ---@param notification Notification
+---@return nil
 function NotificationManager:doShowNotification(notification)
     ---@type string
     local groupId = notification:getGroupId()
-    -- TODO: resolve notification settings here
+    local settings = conf:getSettings(groupId)
+    local displayType = settings:getDisplayType()
+
+    if displayType == NotificationDisplayType.NONE then
+        -- Do nothing TODO: maybe log if logging turned on
+    elseif displayType == NotificationDisplayType.TOOL_WINDOW_BALLOON then
+        -- Do nothing because not implemented TODO: maybe log if logging turned on
+    elseif displayType == NotificationDisplayType.BALLOON or displayType == NotificationDisplayType.STICKY_BALLOON then
+        self:notifyByBalloon(notification, displayType)
+    end
 end
+
+---@private
+---@param notification Notification
+---@param displayType NotificationDisplayType
+function NotificationManager:notifyByBalloon(notification, displayType) end
 
 local manager = NotificationManagerClass:new()
 return manager
